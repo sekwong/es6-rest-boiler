5933da4d75682d2a285312cabc0ada0b9ea10fb6
Eddie <seksan@teamzoon.com>
2017-12-08 09:39:14 +0700

    bug fix add route generator

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import changeCase from 'change-case';
 import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+import mongoose from 'mongoose';
+import restify from 'express-restify-mongoose';
 import routes from './routes';
+import userModel from './models/userModel';
 
 dotenv.config();
 
@@ -15,10 +19,28 @@ app.use(
     extended: true,
   }),
 );
+app.use(methodOverride());
+
+mongoose.connect('mongodb://localhost:27017/database');
+
+// @todo move to auth.js
+app.post('/api/v1/auth/signup', (req, res) => {
+  res.json({ signup: 'ok' });
+});
+
+app.post('/api/v1/auth/signin', (req, res) => {
+  res.json({ signin: 'ok' });
+});
+
+const router = express.Router();
+restify.serve(router, userModel);
+
+app.use(router);
 
 Object.keys(routes).forEach((routeName) => {
-  const router = express.Router();
+  const customerRouter = express.Router();
   routes[routeName].default(router);
-  app.use(`/api/${changeCase.paramCase(routeName)}`, router);
+  app.use(`/api/${changeCase.paramCase(routeName)}`, customerRouter);
 });
+
 export default app;
